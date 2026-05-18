d112cb22bfc95ba2476a3a3a75b6c0029f425147
shekhfarid1974 <shekhfarid1974@gmail.com>
2026-05-18 16:00:34 +0600

    inside app/Providers/appserviceproviders add inside boot defaultstricng leangth 191

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\Schema;
 use App\Repositories\Contracts\CrmRepositoryInterface;
 use App\Repositories\CrmRepository;
 
@@ -22,6 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Schema::defaultStringLength(191);
     }
 }
